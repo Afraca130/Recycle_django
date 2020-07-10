b059d326e32dc12ca987cef783f175be6c3351e4
Sehyeon Kim <zokatn@gmail.com>
2020-07-10 16:49:29 +0900

    make product

--- product/views.py
+++ product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Product
 # Create your views here.
+
+
+class ProductView(ListView):
+    model = Product
+    template_name = 'product.html'
+    context_objcet_name = 'product_list'
