95afde4bdb913f683666895baf016311acc6d9e4
Sehyeon Kim <zokatn@gmail.com>
2020-07-13 19:21:53 +0900

    view2

--- product/forms.py
+++ product/forms.py
@@ -1,54 +1,30 @@
 from django import forms
 from django.contrib.auth.hashers import check_password, make_password
-from .models import User
+from .models import Product
 
 
 class RegisterForm(forms.Form):
-    email = forms.EmailField(
-        error_messages={'required': '이메일을 입력해주세요'}, max_length=64, label='이메일')
-    password = forms.CharField(error_messages={
-                               'required': '비밀번호를 입력해주세요'}, widget=forms.PasswordInput, label='비밀번호')
-    re_password = forms.CharField(error_messages={
-                                  'required': '비밀번호를 입력해주세요'}, widget=forms.PasswordInput, label='비밀번호 확인')
+    name = forms.CharField(
+        error_messages={'required': '상품을 입력해주세요'}, max_length=64, label='상품명')
+    price = forms.IntegerField(error_messages={
+                               'required': '상품가격을 입력해주세요'},  label='상품가격')
+    description = forms.CharField(error_messages={
+                                  'required': '상품정보를 입력해주세요'},  label='상품정보')
+    stock = forms.IntegerField(
+        error_messages={'required': '재고를 입력해주세요'},  label='재고')
 
     def clean(self):
         cleaned_data = super().clean()
-        email = cleaned_data.get('email')
-        password = cleaned_data.get('password')
-        re_password = cleaned_data.get('re_password')
+        name = cleaned_data.get('name')
+        price = cleaned_data.get('price')
+        description = cleaned_data.get('description')
+        stock = cleaned_data.get('stock')
 
-        if password and re_password:
-            if password != re_password:
-                self.add_error('password', '비밀번호가 서로 다릅니다.')
-                self.add_error('re_password', '비밀번호가 서로 다릅니다. ')
-            else:
-                user = User(
-                    email=email,
-                    password=make_password(password)
-                )
-                user.save()
-
-
-class LoginForm(forms.Form):
-    email = forms.EmailField(
-        error_messages={'required': '이메일을 입력해주세요'}, max_length=64, label='이메일')
-    password = forms.CharField(error_messages={
-                               'required': '비밀번호를 입력해주세요'}, widget=forms.PasswordInput, label='비밀번호')
-
-    def clean(self):
-        cleaned_data = super().clean()
-        email = cleaned_data.get('email')
-        password = cleaned_data.get('password')
-
-        if email and password:
-            try:
-                user = User.objects.get(email=email)
-
-            except User.DoesNotExist:
-                self.add_error('email', '이메일이 없습니다.')
-                return
-
-            if not check_password(password, user.password):
-                self.add_error('password', '비밀번호가 틀렸습니다.')
-            else:
-                self.email = user.email
+        if name and price and description and stock:
+            product = Product(
+                name=name,
+                price=price,
+                description=description,
+                stock=stock
+            )
+            product.save()
